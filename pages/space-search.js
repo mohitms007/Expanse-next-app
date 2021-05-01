3e2600b1c17c7a9c28ffc4e262c3ab3c30f0597a
DragonMaster <ms235389@gmail.com>
2021-05-01 14:56:12 +0530

    minor adjustments

--- pages/space-search.js
+++ pages/space-search.js
@@ -2,8 +2,7 @@ import React, {useState} from 'react'
 import {AiOutlineSearch} from 'react-icons/ai'
 import {useRouter} from 'next/router'
 
-export default function SpaceSearch({data,error}) {
-
+export default function SpaceSearch({data, error}) {
 
     const router = useRouter()
     let searchField = ''
@@ -39,7 +38,9 @@ export default function SpaceSearch({data,error}) {
                                             type="search"
                                             name=""
                                             id=""
-                                            onChange={(e) => {searchField = e.target.value}}
+                                            onChange={(e) => {
+                                            searchField = e.target.value
+                                        }}
                                             placeholder="Search for Space-Stories For ex. apollo 11"
                                             x-model="q"
                                             className="w-full pl-4 text-sm outline-none focus:outline-none bg-transparent"/>
@@ -53,30 +54,37 @@ export default function SpaceSearch({data,error}) {
             </div>
             <div className="container mx-auto">
                 <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
-                    { data
-                        ?.map((item,index) => {
+                    {data
+                        ?.map((item, index) => {
                             const {data, links} = item
                             const {title, description_508} = data[0]
                             return (
-                                <div
-                                    key={index}
-                                    className="p-4 sm:p-10 w-96">
+                                <div key={index} className="p-4 sm:p-10 w-96">
 
                                     <div
-                                        className="rounded align-center flex flex-col justify-center h-96 items-center  overflow-hidden shadow-lg">
+                                        className="rounded align-center flex flex-col justify-center h-full items-center  overflow-hidden shadow-lg">
                                         <img
-                                            className="object-cover text-center leading-250 lg:w-96 w-full h-48"
-                                            src={links?.[0].href}
+                                            className="object-cover text-center leading-250 lg:w-96 w-full h-full"
+                                            src={links
+                                            ?.[0].href}
                                             alt=" No Image in the database"/>
                                         <div className="px-6 py-4">
                                             <div className="font-bold text-xl mb-4"></div>
-                                            <p className="text-gray-700 text-center font-bold text-base">
-                                                {title.substring(0,40)}..
+                                            <p className="text-gray-700  font-bold text-base">
+                                                {title.substring(0, 40)}..
                                             </p>
-                                            <p className="text-gray-500 text-center mt-4 text-xs ">
-                                                {data[0]?.description?.substring(0,250)}..
+                                            <p className="text-gray-500  mt-4 text-xs ">
+                                                {data[0]
+                                                    ?.description
+                                                        ?.substring(0, 250)}..
                                             </p>
                                         </div>
+                                        <div className="flex justify-end w-full">
+                                            <button
+                                                className="px-2 py-1 mt-2 mb-4 mx-2 hover:bg-blue-600 text-xs bg-blue-500 text-white font-bold rounded-full focus:outline-none ">View Details</button>
+
+                                        </div>
+
                                     </div>
                                 </div>
                             )
@@ -87,7 +95,9 @@ export default function SpaceSearch({data,error}) {
                 </div>
                 <div className="flex justify-center align-center">
 
-                    <p className="text-center mt-16">{error ? 'No Results found. Please try again.': null}</p>
+                    <p className="text-center mt-16">{error
+                            ? 'No Results found. Please try again.'
+                            : null}</p>
                 </div>
             </div>
 
@@ -101,24 +111,24 @@ export const getServerSideProps = async({query}) => {
     let data = ''
     if (searchQuery) {
         const res = await fetch(`https://images-api.nasa.gov/search?q=${searchQuery}`)
-            data = await res.json()
-            data = data
-                .collection
-                .items
-                .slice(0, 10)
-      
+        data = await res.json()
+        data = data
+            .collection
+            .items
+            .slice(0, 10)
+
     }
     let error;
-    if(data.length == 0){
+    if (data.length == 0) {
         error = true
-    }else{
+    } else {
         error = false
     }
     return {
-        props:{
-            error,data
+        props: {
+            error,
+            data
         }
     }
-  
 
 }
